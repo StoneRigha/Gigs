7b2244733231424bb88c806019c78643b2b18502
Livingstone Maghanga <stoneriga@gmail.com>
2023-04-03 15:04:10 +0300

    controller update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Models\Listing;
@@ -16,16 +17,7 @@ use App\Models\Listing;
 |
 */
 //All listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //single listings
-Route::get('/listings/{listing}', function(Listing $listing){ 
-    return view('listing', [
-        'listing' => $listing
-    ]); 
-});
+Route::get('/listings/{listing}',[ListingController::class, 'show']);
